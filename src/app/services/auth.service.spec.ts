45a00ce617fdc57b310d21183642765d9f63e98c
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-15 22:22:07 +0100

    T3/login working test for the login call

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { AuthService } from './auth.service';
+
+const LOGIN_DATA = {
+  username: 'NAME',
+  password: 'PASS'
+};
+const API_ENDPOINT = 'http://sportsbook-api.azurewebsites.net/api/login';
+const TOKEN = 'TOKEN';
 
 describe('AuthService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientTestingModule],
+    providers: [AuthService]
   }));
 
   it('should be created', () => {
@@ -15,11 +24,41 @@ describe('AuthService', () => {
 
   describe('login() method', () => {
 
-    it('should make a HTTP call to API ');
+    let service: AuthService;
+    let httpMock: HttpTestingController;
+    let httpResponse: string;
+    let httpError: HttpErrorResponse;
+
+    beforeAll(() => {
+      service = TestBed.get(AuthService);
+      httpMock = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+      httpMock.verify();
+    });
+
+    it('should make an HTTP call to the API', () => {
+      service.login(LOGIN_DATA).subscribe((response) => {
+        httpResponse = response;
+      },
+        (error) => {
+          httpError = error;
+        });
+
+      const mockRequest = httpMock.expectOne(API_ENDPOINT);
+
+      expect(mockRequest.cancelled).toBeFalsy();
+      expect(mockRequest.request.responseType).toEqual('text');
+
+      mockRequest.flush(TOKEN);
+      expect(httpResponse).toBe(TOKEN);
+      expect(httpError).toBeUndefined();
+    });
 
     it('should get access token from API');
-
   });
+
 
   describe('isAuthenticated() method', () => {
 
