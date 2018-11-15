95ea566d9440d730f247fe33ab2fd18de8a93e6a
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-15 11:36:28 +0100

    T3/login adding test placeholders to authservice, removing token

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+  }));
 
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
   });
+
+  describe('login() method', () => {
+
+    it('should make a HTTP call to API ');
+
+    it('should get access token from API');
+
+  });
+
+  describe('isAuthenticated() method', () => {
+
+    it('should return false if no access token');
+
+    it('should return true if no access token');
+
+  });
+
 });
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { formModel } from '../login-form/form-model';
 import { environment } from 'src/environments/environment';
@@ -14,22 +14,15 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(loginData: formModel) {
-    this.http.post(environment.API_URL + environment.LOGIN_SUFFIX, loginData, this.getHeaders()).subscribe(
+    this.http.post(environment.API_URL + environment.LOGIN_SUFFIX, loginData).subscribe(
       (value) => { this.token = value.toString(); },
       (error) => {
         console.log(error);
-        // tslint:disable-next-line:max-line-length
-        this.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJuYmYiOjE1NDIyNjI0NjEsImV4cCI6MTU0Mjg2NzI2MSwiaWF0IjoxNTQyMjYyNDYxfQ.AWMvhC1xeG5kEhPWKiOKwU5Fhtvd5LyiMzIt5rMDtJE';
       }
     );
   }
 
   public isAuthenticated() {
     return !!this.token;
-  }
-
-  private getHeaders() {
-    const headers = new HttpHeaders();
-    return { headers: headers };
   }
 }
