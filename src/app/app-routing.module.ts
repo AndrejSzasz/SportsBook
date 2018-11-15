e20887111b0e4c460f993a8e990b0666d534925a
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-15 11:39:30 +0100

    T3/login adding guards (tests WIP)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { LoggedInGuard } from './guards/logged-in.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginFormComponent }
+  {
+    path: 'test',
+    component: LoginFormComponent,
+    canActivate: [LoggedInGuard]
+  },
+  { path: 'login', component: LoginFormComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
