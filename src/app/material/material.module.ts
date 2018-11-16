2cd97ced56b9bc4b4d27b032b02608888f09719e
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-16 14:21:43 +0100

    T4/stadiums Added material styling to list component

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatListModule,
+  } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    ],
+})
+
+export class MaterialModule { }
