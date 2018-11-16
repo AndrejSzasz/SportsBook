88fc8f0b945060116765d89a49e581c3ebe6d304
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-16 12:06:17 +0100

    T4/stadiums fix tests for httpservice get()

--- src/app/services/sb-http.service.spec.ts
+++ src/app/services/sb-http.service.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed, fakeAsync } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { SbHttpService } from './sb-http.service';
 import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
 
 const AuthStubService = {
   token: 'access-token',
@@ -10,7 +12,11 @@ const AuthStubService = {
   isAuthenticated: function () { }
 };
 
-const stubData = [
+interface Test {
+  id: string;
+  name: string;
+}
+const stubData: Array<Test> = [
   {
     id: '1',
     name: 'whatever'
@@ -24,30 +30,51 @@ const stubData = [
 const GET_ENDPOINT = '/fake/get';
 
 describe('SbHttpService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ],
-    providers: [
-      { provide: AuthService, useValue: AuthStubService }
-    ]
-  }));
+  let service: SbHttpService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        { provide: AuthService, useValue: AuthStubService }
+      ]
+    });
+    service = TestBed.get(SbHttpService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: SbHttpService = TestBed.get(SbHttpService);
     expect(service).toBeTruthy();
   });
 
-  xdescribe('get() method', () => {
-    it('should call the api URL ', fakeAsync(() => {
-      const service: SbHttpService = TestBed.get(SbHttpService);
-      const httpMock = TestBed.get(HttpTestingController);
-      service.get(GET_ENDPOINT).subscribe(
-        () => {
-          const calledUrl = httpMock.expectOne(GET_ENDPOINT);
-          expect(calledUrl).toBe('GET_ENDPOINT');
+  describe('get() method', () => {
+    it('should call the api URL ', () => {
+      let response: Array<Test>;
+      let httpError: HttpErrorResponse;
+
+      service.get<Array<Test>>(GET_ENDPOINT).subscribe(
+        (value) => {
+          console.log(value);
+          response = value;
+        },
+        (error) => {
+          httpError = error;
         }
       );
-    }));
+      const mockRequest = httpMock.expectOne(environment.API_URL + GET_ENDPOINT);
+      expect(mockRequest.cancelled).toBeFalsy();
+      expect(mockRequest.request.responseType).toEqual('json');
+
+      mockRequest.flush(stubData);
+      expect(response).toEqual(stubData);
+      expect(httpError).toBeUndefined('should NOT return an error');
+    });
   });
 });
