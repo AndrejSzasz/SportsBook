a753c9f480f0f43503d254b25754dcfb8ee84d76
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-22 21:03:39 +0100

    issue#9/add-event Component updated with error messages, working stadium list (async pipe did not work).

--- src/app/events/add-event/add-event.component.spec.ts
+++ src/app/events/add-event/add-event.component.spec.ts
@@ -1,6 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../material/material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 import { AddEventComponent } from './add-event.component';
+import { EventService } from '../event.service';
+import { StadiumService, Stadium } from '../../stadiums/stadium.service';
+
+let eventResponse: number;
+let stadiumResponse: Array<Stadium>;
+
+const EventStubService = {
+  init: function () { },
+  addEvent: function () {
+    return of(eventResponse);
+  },
+};
+
+const StadiumStubService = {
+  stadiums$: of(stadiumResponse),
+  init: function () { },
+};
+
 
 describe('AddEventComponent', () => {
   let component: AddEventComponent;
@@ -8,7 +30,17 @@ describe('AddEventComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddEventComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+        NoopAnimationsModule,
+      ],
+      declarations: [ AddEventComponent ],
+      providers: [
+        { provide: EventService, useValue: EventStubService },
+        { provide: StadiumService, useValue: StadiumStubService },
+      ]
     })
     .compileComponents();
   }));
--- src/app/events/add-event/add-event.component.ts
+++ src/app/events/add-event/add-event.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { EventService } from '../event.service';
-import { StadiumService } from 'src/app/stadiums/stadium.service';
+import { StadiumService, Stadium } from 'src/app/stadiums/stadium.service';
 
 @Component({
   selector: 'sb-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.scss']
 })
-export class AddEventComponent implements OnInit {
+export class AddEventComponent implements OnInit, OnDestroy {
 
   addForm: FormGroup;
   showAdd: boolean;
   @Output() close = new EventEmitter();
+  stadiums: Array<Stadium>;
+  destroy$: Subject<void> = new Subject();
 
   constructor(
     public eventService: EventService,
@@ -26,10 +30,34 @@ export class AddEventComponent implements OnInit {
   ngOnInit() {
     // Form setup corresponds to API model
     this.addForm = new FormGroup({
-      name: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
       startTime: new FormControl('', [Validators.required]),
       stadiumId: new FormControl('', [Validators.required]),
     });
+    this.stadiumService.stadiums$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (result) => {
+          this.stadiums = result;
+        },
+        (error) => {
+          if (error instanceof HttpErrorResponse) {
+            this.snackBar.open(error.error, 'Dismiss');
+          } else {
+            throw error;
+          }
+        },
+        () => {
+          console.log('stadiumService observable completed!');
+        }
+      );
+    this.stadiumService.init();
+    this.showAdd = false;
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   onSubmit() {
