5f4ada6bc36694bf404cfff4e767dafa3ed9c113
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-22 11:04:14 +0100

    issue#9/add-event add-event stub

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -4,11 +4,13 @@ import { MaterialModule } from '../material/material.module';
 
 import { ListEventsComponent } from './list-events/list-events.component';
 import { EventListItemComponent } from './event-list-item/event-list-item.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 @NgModule({
   declarations: [
     ListEventsComponent,
     EventListItemComponent,
+    AddEventComponent,
   ],
   imports: [
     CommonModule,
