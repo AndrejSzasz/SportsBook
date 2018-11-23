69ec0ba78a488c95b72ea0a89f38abaee84af0ca
Andrej Szasz <Andrej_Szasz@epam.com>
2018-11-23 09:52:11 +0100

    issue#9/add-event just make linter not complain. new test added but does not work.

--- src/app/events/add-event/add-event.component.spec.ts
+++ src/app/events/add-event/add-event.component.spec.ts
@@ -4,6 +4,7 @@ import { MaterialModule } from '../../material/material.module';
 import { MatDialogRef } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 import { AddEventComponent } from './add-event.component';
 import { EventService } from '../event.service';
@@ -37,14 +38,14 @@ describe('AddEventComponent', () => {
         MaterialModule,
         NoopAnimationsModule,
       ],
-      declarations: [ AddEventComponent ],
+      declarations: [AddEventComponent],
       providers: [
         { provide: EventService, useValue: EventStubService },
         { provide: StadiumService, useValue: StadiumStubService },
         { provide: MatDialogRef, useValue: {} },
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -55,5 +56,31 @@ describe('AddEventComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  xit('should display the stadiums in a drop-down', () => {
+    // decided not to test it, as it tests angular material
+    // should only test the binding between api response and on-screen real estate
+    // but angular material does not have an options HTML, it just emulates the look
+    const NAME = 'NAME';
+    stadiumResponse = [
+      {
+        id: 1,
+        name: NAME,
+      }
+    ];
+    const select = fixture.debugElement.query(By.css('mat-select'));
+    // trigger click on UI
+    expect(select).toBeTruthy('can not find select box');
+    select.nativeElement.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    const options = fixture.debugElement.queryAll(By.css('mat-option'));
+    expect(options.length).toBe(1);
+    console.log(options[0].nativeElement);
+    expect(options[0].nativeElement.textContent).toBe(NAME);
+  });
+
+  xit('should close the dialog on success', () => {
+    eventResponse = 1;
   });
 });
